4f33a7906d38b1cd27c836ffe8953e25feda4c13
BuiGiaUy <101172225+BuiGiaUy@users.noreply.github.com>
2024-05-18 15:54:51 +0700

    feat: install file manager

--- website/resources/js/app.js
+++ website/resources/js/app.js
@@ -13,6 +13,21 @@ window.addEventListener('DOMContentLoaded', () => {
         selector: 'textarea',
         /* Cấu hình tinyMCE */
         skin: false,
-        content_css: false
+        content_css: false,
+
+        file_picker_callback (callback, value, meta) {
+            let x = window.innerWidth || document.documentElement.clientWidth || document.getElementsByTagName('body')[0].clientWidth
+            let y = window.innerHeight|| document.documentElement.clientHeight|| document.getElementsByTagName('body')[0].clientHeight
+
+            tinymce.activeEditor.windowManager.openUrl({
+                url : '/file-manager/tinymce5',
+                title : 'Laravel File manager',
+                width : x * 0.8,
+                height : y * 0.8,
+                onMessage: (api, message) => {
+                    callback(message.content, { text: message.text })
+                }
+            })
+        }
     });
 });
--- website/resources/views/tinymce.blade.php
+++ website/resources/views/tinymce.blade.php
@@ -5,6 +5,14 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     @vite(['resources/js/app.js'])
+    <meta name="csrf-token" content="{{ csrf_token() }}">
+
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css">
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.0/css/all.css">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.2/font/bootstrap-icons.min.css">
+
+    <link rel="stylesheet" href="{{ asset('vendor/file-manager/css/file-manager.css') }}">
+    <script src="{{ asset('vendor/file-manager/js/file-manager.js') }}"></script>
 </head>
 <body>
 <h2>Laracoding.com - TinyMCE Example</h2>
@@ -14,6 +22,32 @@
         <textarea class="tinyMce" name="user-bio"></textarea>
     </div>
     <button type="submit" class="btn btn-primary">Submit</button>
+    <div id="fm" style="height: 600px;"></div>
+
 </form>
+<div class="input-group">
+    <input type="text" id="image_label" class="form-control" name="image"
+           aria-label="Image" aria-describedby="button-image">
+    <div class="input-group-append">
+        <button class="btn btn-outline-secondary" type="button" id="button-image">Select</button>
+    </div>
+</div>
+<script>
+
+    document.addEventListener("DOMContentLoaded", function() {
+
+        document.getElementById('button-image').addEventListener('click', (event) => {
+            event.preventDefault();
+
+            window.open('/file-manager/fm-button', 'fm', 'width=1400,height=800');
+        });
+    });
+
+    // set file link
+    function fmSetLink($url) {
+// cấu hình link
+        document.getElementById('image_label').value = $url;
+    }
+</script>
 </body>
 </html>
