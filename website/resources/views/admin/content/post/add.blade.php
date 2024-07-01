50a83fc69391877f130a48c645aed4b350278e34
BuiGiaUy <101172225+BuiGiaUy@users.noreply.github.com>
2024-07-01 09:16:34 +0700

    update button image file manager

--- website/resources/views/admin/content/post/add.blade.php
+++ website/resources/views/admin/content/post/add.blade.php
@@ -14,12 +14,64 @@
             <h2 class="text-2xl font-semibold mb-6">Add New Post</h2>
             <form action="{{ route('admin.post.store') }}" method="POST" enctype="multipart/form-data">
                 @csrf
-                <div class="grid grid-cols-1 md:grid-cols-2 gap-6">
-                    <!-- Title -->
-                    <div class="col-span-2">
-                        <label for="title" class="block text-gray-700 font-medium mb-2">Title</label>
-                        <input type="text" name="title" id="title" class="form-input w-full" placeholder="Enter title...">
+                <div class="grid grid-cols-12 gap-6 mt-5">
+                    <div class="intro-y col-span-12 lg:col-span-6">
+                        <!-- Title -->
+                        <div class="">
+                            <label for="title" class="block text-gray-700 font-medium mb-2">Title</label>
+                            <input type="text" name="title" id="title" class="form-input w-full" placeholder="Enter title...">
+                            @error('title')
+                            <p class="text-red-500 text-xs mt-1">{{ $message }}</p>
+                            @enderror
+                        </div>
+                        <!-- Content -->
+                        <div class="">
+                            <label for="content" class="block text-gray-700 font-medium mb-2">Content</label>
+                            <textarea name="content" id="content" class="form-textarea w-full" rows="8" placeholder="Enter content..."></textarea>
+                            @error('content')
+                            <p class="text-red-500 text-xs mt-1">{{ $message }}</p>
+                            @enderror
+                        </div>
+                        <!-- Description -->
+                        <div class="">
+                            <label for="description" class="block text-gray-700 font-medium mb-2">Description</label>
+                            <textarea name="description" id="description" class="form-textarea w-full" rows="4" placeholder="Enter description..."></textarea>
+                            @error('description')
+                            <p class="text-red-500 text-xs mt-1">{{ $message }}</p>
+                            @enderror
+                        </div>
+
                     </div>
+                    <div class="intro-y col-span-12 lg:col-span-6">
+                        <!-- SEO Title -->
+                        <div>
+                            <label for="seo_title" class="block text-gray-700 font-medium mb-2">SEO Title</label>
+                            <input type="text" name="seo_title" id="seo_title" class="form-input w-full" placeholder="Enter SEO title...">
+                            @error('seo_title')
+                            <p class="text-red-500 text-xs mt-1">{{ $message }}</p>
+                            @enderror
+                        </div>
+                        <!-- SEO Keywords -->
+                        <div>
+                            <label for="seo_keywords" class="block text-gray-700 font-medium mb-2">SEO Keywords</label>
+                            <textarea type="text" name="seo_keywords" id="seo_keywords" class="form-input w-full" placeholder="Enter SEO keywords...">
+                            </textarea>
+                            @error('seo_keywords')
+                            <p class="text-red-500 text-xs mt-1">{{ $message }}</p>
+                            @enderror
+                        </div>
+                        <!-- SEO Description -->
+                        <div class="">
+                            <label for="seo_description" class="block text-gray-700 font-medium mb-2">SEO Description</label>
+                            <textarea name="seo_description" id="seo_description" class="form-textarea w-full" rows="4" placeholder="Enter SEO description..."></textarea>
+                            @error('seo_description')
+                            <p class="text-red-500 text-xs mt-1">{{ $message }}</p>
+                            @enderror
+                        </div>
+                    </div>
+                </div>
+
+                <div class="grid grid-cols-1 md:grid-cols-2 gap-6">
                     <!-- Categories -->
                     <div>
                         <label for="category" class="block text-gray-700 font-medium mb-2">Categories</label>
@@ -29,59 +81,75 @@
                                 <option value="{{ $category->id }}">{{ $category->name }}</option>
                             @endforeach
                         </select>
+                        @error('category')
+                        <p class="text-red-500 text-xs mt-1">{{ $message }}</p>
+                        @enderror
                     </div>
                     <!-- Slug -->
                     <div>
                         <label for="slug" class="block text-gray-700 font-medium mb-2">Slug</label>
                         <input type="text" name="slug" id="slug" class="form-input w-full" placeholder="Enter slug...">
+                        @error('slug')
+                        <p class="text-red-500 text-xs mt-1">{{ $message }}</p>
+                        @enderror
                     </div>
                     <!-- Upload Image -->
                     <div class="col-span-2">
                         <label for="image" class="block text-gray-700 font-medium mb-2">Upload Image</label>
                         <div class="border-2 border-dashed dark:border-darkmode-400 rounded-md pt-4">
-                            <div class="flex flex-wrap px-4">
-                                <div class="w-24 h-24 relative image-fit mb-5 mr-5 cursor-pointer zoom-in">
-                                    <img class="rounded-md" alt="Midone - HTML Admin Template" src="{{ asset('backend/dist/images/preview-9.jpg') }}">
-                                    <div title="Remove this image?" class="tooltip w-5 h-5 flex items-center justify-center absolute rounded-full text-white bg-danger right-0 top-0 -mr-2 -mt-2"> <i data-lucide="x" class="w-4 h-4"></i> </div>
+                            <div class="flex flex-wrap px-4" id="add-images">
+
+                                <div class=" w-24 h-24 relative image-fit mb-5 mr-5 cursor-pointer zoom-in" id="button-image">
+                                    <img class="text-white rounded-md" alt="Midone - HTML Admin Template" src="{{ asset('images/cong2.jpg') }}">
                                 </div>
-                            </div>
-                            <div class="px-4 pb-4 flex items-center cursor-pointer relative">
-                                <i data-lucide="image" class="w-4 h-4 mr-2"></i> <span class="text-primary mr-1">Upload a file</span> or drag and drop
-                                <input type="file" class="w-full h-full top-0 left-0 absolute opacity-0" name="image">
+
                             </div>
                         </div>
-                    </div>
-                    <!-- Description -->
-                    <div class="col-span-2">
-                        <label for="description" class="block text-gray-700 font-medium mb-2">Description</label>
-                        <textarea name="description" id="description" class="form-textarea w-full" rows="4" placeholder="Enter description..."></textarea>
-                    </div>
-                    <!-- Content -->
-                    <div class="col-span-2">
-                        <label for="content" class="block text-gray-700 font-medium mb-2">Content</label>
-                        <textarea name="content" id="content" class="form-textarea w-full" rows="8" placeholder="Enter content..."></textarea>
-                    </div>
-                    <!-- SEO Title -->
-                    <div>
-                        <label for="seo_title" class="block text-gray-700 font-medium mb-2">SEO Title</label>
-                        <input type="text" name="seo_title" id="seo_title" class="form-input w-full" placeholder="Enter SEO title...">
-                    </div>
-                    <!-- SEO Keywords -->
-                    <div>
-                        <label for="seo_keywords" class="block text-gray-700 font-medium mb-2">SEO Keywords</label>
-                        <input type="text" name="seo_keywords" id="seo_keywords" class="form-input w-full" placeholder="Enter SEO keywords...">
-                    </div>
-                    <!-- SEO Description -->
-                    <div class="col-span-2">
-                        <label for="seo_description" class="block text-gray-700 font-medium mb-2">SEO Description</label>
-                        <textarea name="seo_description" id="seo_description" class="form-textarea w-full" rows="4" placeholder="Enter SEO description..."></textarea>
+                        @error('image')
+                        <p class="text-red-500 text-xs mt-1">{{ $message }}</p>
+                        @enderror
                     </div>
                 </div>
                 <!-- Submit Button -->
                 <div class="mt-5 text-right">
-                    <button type="submit" class="btn btn-primary">Save post</button>
+                    <button type="submit" class="btn btn-primary">Save Post</button>
                 </div>
             </form>
         </div>
     </div>
+    <script>
+
+        document.addEventListener("DOMContentLoaded", function() {
+
+            document.getElementById('button-image').addEventListener('click', (event) => {
+                event.preventDefault();
+
+                window.open('/file-manager/fm-button', 'fm', 'width=1400,height=800');
+            });
+            document.getElementById('add-images').addEventListener('click', function(event) {
+                if (event.target.classList.contains('close-button')) {
+                    event.target.closest('.image-container').remove();
+                }
+            });
+        });
+
+        // set file link
+        function fmSetLink($url) {
+            let newUrl = $url.replace("http://localhost/", "http://winewebsite.th/");
+            // cấu hình link
+            // document.getElementById('image_label').value = newUrl;
+            let newHtml = `<div class="w-24 h-24 relative image-fit mb-5 mr-5 cursor-pointer zoom-in image-container">
+                        <img class="rounded-md" alt="Midone - HTML Admin Template" src="${newUrl}">
+                        <div class="tooltip w-5 h-5 flex items-center justify-center absolute rounded-full text-white bg-danger right-0 top-0 -mr-2 -mt-2 close-button">
+                            x
+                        </div>
+                   </div>
+                        `;
+            // Get the element with the ID 'add-images'
+            let addImagesElement = document.getElementById('add-images');
+
+            // Insert the new HTML at the beginning of the element
+            addImagesElement.insertAdjacentHTML('afterbegin', newHtml);
+        }
+    </script>
 @endsection
--- website/resources/views/admin/content/product/add.blade.php
+++ website/resources/views/admin/content/product/add.blade.php
@@ -9,73 +9,190 @@
     </nav>
 @endsection
 @section('content')
-    <div class="intro-y flex flex-col sm:flex-row items-center mt-8">
-        <h2 class="text-lg font-medium mr-auto">
-            Add New Product
-        </h2>
-    </div>
-    <div class="pos intro-y grid grid-cols-12 gap-5 mt-5">
-        <div class="intro-y col-span-12 lg:col-span-6">
+    <div class="container mx-auto py-8">
+        <div class="">
+            <h2 class="text-2xl font-semibold mb-6">Add New Post</h2>
             <form action="{{ route('admin.product.store') }}" method="POST">
-                @csrf
-                <div class="box p-5">
-                    <div>
-                        <label class="form-label">Category</label>
-                        <select name="category_id" class="form-control">
-                            @foreach($categories as $category)
-                                <option value="{{ $category->id }}">{{ $category->name }}</option>
-                            @endforeach
-                        </select>
+            @csrf
+                <div class="pos intro-y grid grid-cols-12 gap-5 mt-5">
+                    <div class="intro-y col-span-12 lg:col-span-6">
+                            <div class="box p-5">
+                                <div>
+                                    <label class="form-label">Category</label>
+                                    <select name="category_id" class="form-control">
+                                        @foreach($categories as $category)
+                                            <option value="{{ $category->id }}">{{ $category->name }}</option>
+                                        @endforeach
+                                    </select>
+                                </div>
+                                <div class="mt-3">
+                                    <label class="form-label">Product Name</label>
+                                    <input type="text" name="name" class="form-control" placeholder="Product Name" required>
+                                </div>
+                                <div class="mt-3">
+                                    <label class="form-label">Slug</label>
+                                    <input type="text" name="slug" class="form-control" placeholder="Slug" required>
+                                </div>
+                                <div class="mt-3">
+                                    <label class="form-label">Barcode</label>
+                                    <input type="text" name="barcode" class="form-control" placeholder="Barcode" required>
+                                </div>
+                                <div class="mt-3">
+                                    <label class="form-label">Description</label>
+                                    <textarea name="description" class="form-control" placeholder="Description" required></textarea>
+                                </div>
+                            </div>
                     </div>
-                    <div class="mt-3">
-                        <label class="form-label">Product Name</label>
-                        <input type="text" name="name" class="form-control" placeholder="Product Name" required>
+                    <div class="intro-y col-span-12 lg:col-span-6">
+                        <div class="box p-5">
+                            <div class="mt-3">
+                                <label class="form-label">Quantity</label>
+                                <input type="number" name="quantity" class="form-control" placeholder="Quantity" required>
+                            </div>
+                            <div class="mt-3">
+                                <label class="form-label">Price</label>
+                                <input type="number" name="price" class="form-control" placeholder="Price" required>
+                            </div>
+                            <div class="mt-3">
+                                <label class="form-label">Discount Percent</label>
+                                <input type="number" name="discount_percent" class="form-control" placeholder="Discount Percent" required>
+                            </div>
+                            <div class="mt-3">
+                                <label class="form-label">Brand</label>
+                                <select name="brand_id" class="form-control">
+                                    @foreach($brands as $brand)
+                                        <option value="{{ $brand->id }}">{{ $brand->name }}</option>
+                                    @endforeach
+                                </select>
+                            </div>
+                            <div class="mt-3">
+                                <label class="form-label">Post</label>
+                                <select name="post_id" class="form-control">
+                                    @foreach($posts as $post)
+                                        <option value="{{ $post->id }}">{{ $post->title }}</option>
+                                    @endforeach
+                                </select>
+                            </div>
+                            <div class="grid grid-cols-1 md:grid-cols-2 gap-6">
+
+                                <!-- Upload Image -->
+                                <div class="col-span-2">
+                                    <label for="image" class="block text-gray-700 font-medium mb-2">Upload Image</label>
+                                    <div class="border-2 border-dashed dark:border-darkmode-400 rounded-md pt-4">
+                                        <div class="flex flex-wrap px-4" id="add-images">
+
+                                            <div class=" w-24 h-24 relative image-fit mb-5 mr-5 cursor-pointer zoom-in" id="button-image">
+                                                <img class="text-white rounded-md" alt="Midone - HTML Admin Template" src="{{ asset('images/cong2.jpg') }}">
+                                            </div>
+
+                                        </div>
+                                    </div>
+                                    @error('image')
+                                    <p class="text-red-500 text-xs mt-1">{{ $message }}</p>
+                                    @enderror
+                                </div>
+                            </div>
+                        </div>
                     </div>
-                    <div class="mt-3">
-                        <label class="form-label">Slug</label>
-                        <input type="text" name="slug" class="form-control" placeholder="Slug" required>
+                </div>
+
+                <div class="grid grid-cols-12 gap-6 mt-5 bg-white rounded-lg shadow-md p-8">
+                    <div class="intro-y col-span-12 lg:col-span-6">
+                        <!-- Title -->
+                        <div class="">
+                            <label for="title" class="block text-gray-700 font-medium mb-2">Title</label>
+                            <input type="text" name="title" id="title" class="form-input w-full" placeholder="Enter title...">
+                            @error('title')
+                            <p class="text-red-500 text-xs mt-1">{{ $message }}</p>
+                            @enderror
+                        </div>
+                        <!-- Content -->
+                        <div class="">
+                            <label for="content" class="block text-gray-700 font-medium mb-2">Content</label>
+                            <textarea name="content" id="content" class="form-textarea w-full" rows="8" placeholder="Enter content..."></textarea>
+                            @error('content')
+                            <p class="text-red-500 text-xs mt-1">{{ $message }}</p>
+                            @enderror
+                        </div>
+                        <!-- Description -->
+                        <div class="">
+                            <label for="description" class="block text-gray-700 font-medium mb-2">Description</label>
+                            <textarea name="description" id="description" class="form-textarea w-full" rows="4" placeholder="Enter description..."></textarea>
+                            @error('description')
+                            <p class="text-red-500 text-xs mt-1">{{ $message }}</p>
+                            @enderror
+                        </div>
+
                     </div>
-                    <div class="mt-3">
-                        <label class="form-label">Barcode</label>
-                        <input type="text" name="barcode" class="form-control" placeholder="Barcode" required>
+                    <div class="intro-y col-span-12 lg:col-span-6">
+                        <!-- SEO Title -->
+                        <div>
+                            <label for="seo_title" class="block text-gray-700 font-medium mb-2">SEO Title</label>
+                            <input type="text" name="seo_title" id="seo_title" class="form-input w-full" placeholder="Enter SEO title...">
+                            @error('seo_title')
+                            <p class="text-red-500 text-xs mt-1">{{ $message }}</p>
+                            @enderror
+                        </div>
+                        <!-- SEO Keywords -->
+                        <div>
+                            <label for="seo_keywords" class="block text-gray-700 font-medium mb-2">SEO Keywords</label>
+                            <textarea type="text" name="seo_keywords" id="seo_keywords" class="form-input w-full" placeholder="Enter SEO keywords...">
+                            </textarea>
+                            @error('seo_keywords')
+                            <p class="text-red-500 text-xs mt-1">{{ $message }}</p>
+                            @enderror
+                        </div>
+                        <!-- SEO Description -->
+                        <div class="">
+                            <label for="seo_description" class="block text-gray-700 font-medium mb-2">SEO Description</label>
+                            <textarea name="seo_description" id="seo_description" class="form-textarea w-full" rows="4" placeholder="Enter SEO description..."></textarea>
+                            @error('seo_description')
+                            <p class="text-red-500 text-xs mt-1">{{ $message }}</p>
+                            @enderror
+                        </div>
                     </div>
-                    <div class="mt-3">
-                        <label class="form-label">Description</label>
-                        <textarea name="description" class="form-control" placeholder="Description" required></textarea>
-                    </div>
-                    <div class="mt-3">
-                        <label class="form-label">Quantity</label>
-                        <input type="number" name="quantity" class="form-control" placeholder="Quantity" required>
-                    </div>
-                    <div class="mt-3">
-                        <label class="form-label">Price</label>
-                        <input type="number" name="price" class="form-control" placeholder="Price" required>
-                    </div>
-                    <div class="mt-3">
-                        <label class="form-label">Discount Percent</label>
-                        <input type="number" name="discount_percent" class="form-control" placeholder="Discount Percent" required>
-                    </div>
-                    <div class="mt-3">
-                        <label class="form-label">Brand</label>
-                        <select name="brand_id" class="form-control">
-                            @foreach($brands as $brand)
-                                <option value="{{ $brand->id }}">{{ $brand->name }}</option>
-                            @endforeach
-                        </select>
-                    </div>
-                    <div class="mt-3">
-                        <label class="form-label">Post</label>
-                        <select name="post_id" class="form-control">
-                            @foreach($posts as $post)
-                                <option value="{{ $post->id }}">{{ $post->title }}</option>
-                            @endforeach
-                        </select>
-                    </div>
-                    <div class="mt-5 text-right">
-                        <button type="submit" class="btn btn-primary">Save Product</button>
-                    </div>
+                </div>
+
+                <!-- Submit Button -->
+                <div class="mt-5 text-right">
+                    <button type="submit" class="btn btn-primary">Save Product</button>
                 </div>
             </form>
         </div>
     </div>
+    <script>
+
+        document.addEventListener("DOMContentLoaded", function() {
+
+            document.getElementById('button-image').addEventListener('click', (event) => {
+                event.preventDefault();
+
+                window.open('/file-manager/fm-button', 'fm', 'width=1400,height=800');
+            });
+            document.getElementById('add-images').addEventListener('click', function(event) {
+                if (event.target.classList.contains('close-button')) {
+                    event.target.closest('.image-container').remove();
+                }
+            });
+        });
+
+        // set file link
+        function fmSetLink($url) {
+            let newUrl = $url.replace("http://localhost/", "http://winewebsite.th/");
+            // cấu hình link
+            // document.getElementById('image_label').value = newUrl;
+            let newHtml = `<div class="w-24 h-24 relative image-fit mb-5 mr-5 cursor-pointer zoom-in image-container">
+                        <img class="rounded-md" alt="Midone - HTML Admin Template" src="${newUrl}">
+                        <div class="tooltip w-5 h-5 flex items-center justify-center absolute rounded-full text-white bg-danger right-0 top-0 -mr-2 -mt-2 close-button">
+                            x
+                        </div>
+                   </div>
+                        `;
+            // Get the element with the ID 'add-images'
+            let addImagesElement = document.getElementById('add-images');
+
+            // Insert the new HTML at the beginning of the element
+            addImagesElement.insertAdjacentHTML('afterbegin', newHtml);
+        }
+    </script>
 @endsection
