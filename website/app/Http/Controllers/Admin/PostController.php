90f9bb9af3b5997a0a7214dd5f81fd8683c526c7
BuiGiaUy <101172225+BuiGiaUy@users.noreply.github.com>
2024-07-01 20:39:32 +0700

    feat_adminLayout

--- website/app/Http/Controllers/Admin/PostController.php
+++ website/app/Http/Controllers/Admin/PostController.php
@@ -52,7 +52,7 @@ class PostController extends Controller
         return view('admin.content.post.index', [
             "posts" => Post::orderBy('category_id', 'ASC')->whereHas('category', function ($query) use($group) {
                 $query->where('model_type', $group);
-            })->qaginate(15)
+            })->paginate(50)
         ]);
 
 //        $posts = Post::orderBy('category_id', 'ASC')->whereHas('category', function ($query) use ($group) {
