acc17cf8bd8bf43d921aaabc74c99e08975613a9
BuiGiaUy <101172225+BuiGiaUy@users.noreply.github.com>
2024-07-17 05:11:06 +0700

    Resolve conflicts in admin routes

--- website/routes/admin.php
+++ website/routes/admin.php
@@ -7,8 +7,6 @@ use App\Http\Controllers\Admin\CategoryController;
 use App\Http\Controllers\Admin\HomeController;
 use App\Http\Controllers\Admin\OrderController;
 use App\Http\Controllers\Admin\PostController;
-use App\Http\Controllers\Admin\ProductController;
-use App\Http\Controllers\Admin\UserController;
 use Illuminate\Support\Facades\Route;
 
 Route::namespace('admin')->group(function () {
