e7e2021208e631b21e31ba03c12543c9f2252d33
BuiGiaUy <101172225+BuiGiaUy@users.noreply.github.com>
2024-07-17 05:16:55 +0700

    ss

--- website/routes/admin.php
+++ website/routes/admin.php
@@ -7,6 +7,8 @@ use App\Http\Controllers\Admin\CategoryController;
 use App\Http\Controllers\Admin\HomeController;
 use App\Http\Controllers\Admin\OrderController;
 use App\Http\Controllers\Admin\PostController;
+use App\Http\Controllers\Admin\ProductController;
+use App\Http\Controllers\Admin\UserController;
 use Illuminate\Support\Facades\Route;
 
 Route::namespace('admin')->group(function () {
